035930f582121de31dee751af869b618e83d0198
Sushith04 <sushithreddy21@gmail.com>
2022-09-25 03:41:11 -0400

    assignmnet 2 initial commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h2>
+          This is assignment 2
+        </h2>
         <a
           className="App-link"
           href="https://reactjs.org"
