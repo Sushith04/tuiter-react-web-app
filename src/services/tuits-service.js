a5dfdf44eccbf1618c463fa4c5654355e75c4047
Sushith04 <sushithreddy21@gmail.com>
2022-11-30 21:55:07 -0500

    a9 Mongo fa22

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const API_BASE = process.env.REACT_APP_API_BASE;
-const TUITS_API = `${API_BASE}/tuits`;
+const TUITS_API = API_BASE || `http://localhost:4000/api/tuits`;
 
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
@@ -16,7 +16,7 @@ export const findTuits = async () => {
 export const deleteTuit = async (tid) => {
     const response = await axios
         .delete(`${TUITS_API}/${tid}`)
-    return response.data
+    return response.data;
 }
 
 export const updateTuit = async (tuit) => {
