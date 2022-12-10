6261876a6d3da7c1c44ff0b6de2359e2ff800611
Sushith04 <sushithreddy21@gmail.com>
2022-12-10 08:44:17 -0500

    redeploy a9

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
-const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:4000/api";
+const API_BASE = process.env.REACT_APP_API_BASE_A9 || "http://localhost:4000/api";
 console.log(process.env.REACT_APP_API_BASE)
 console.log(process.env.REACT_APP_API_BASE_A9)
+
 const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
